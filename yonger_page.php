be47544a6ac18a5d04ea48084cce93a1e6eaf2c7
Oleg Frolov <oleg_frolov@mail.ru>
2021-10-06 14:51:16 +0300

    update

--- yonger_page.php
+++ yonger_page.php
@@ -59,9 +59,9 @@ class yongerPage {
     function blockform($item = []) {
         if (!isset($item['form'])) return;
         $file = $item['form'];
-        strpos(' '.$file,'_yonger_') ? $file = str_replace('/_yonger_',$this->app->route->path_app.'/modules/yonger',$file) : null;
+        strpos(' '.$file,'_yonger_') ? $file = str_replace('/_yonger_',__DIR__,$file) : null;
         strpos(' '.$file, '_app_') ? $file = str_replace('/_app_',$this->app->route->path_app, $file) : null;
-        is_file($file) ? null : $file = $this->app->route->path_app.'/modules/yonger/common/blocks/'.$file;
+        is_file($file) ? null : $file = __DIR__ .'/common/blocks/'.$file;
         $out = $this->app->fromFile($file);
         $out->fetch($item);
         $out = $out->find('edit');
@@ -73,7 +73,7 @@ class yongerPage {
     function blockview($file = null) {
         if ($file == null) return;
         $src = $file;
-        strpos(' '.$file,'_yonger_') ? $file = str_replace('/_yonger_',$this->app->route->path_app.'/modules/yonger',$file) : null;
+        strpos(' '.$file,'_yonger_') ? $file = str_replace('/_yonger_',__DIR__,$file) : null;
         strpos(' '.$file, '_app_') ? $file = str_replace('/_app_',$this->app->route->path_app, $file) : null;
         !is_file($file) ? $file = __DIR__ . '/common/blocks/'.$file : null; // если не прописане полный путь
         if (!is_file($file)) {
